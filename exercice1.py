b61cc4affe07209e5ae0a9e509b37ef2dbce1b37
Justin Lefrançois <lefrancoisjustin@gmail.com>
2021-09-14 12:16:40 -0400

    Fait 1,2,3

--- exercice1.py
+++ exercice1.py
@@ -1,8 +1,24 @@
 def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
+    fizz = 0
+    buzz = 0
+    reste = n % 3
+    resultat = n
 
-    resultat =
+    if reste == 0 :
+        resultat = "fizz"
+        fizz = 1
+
+    reste = n % 5
+
+    if reste == 0 :
+        resultat = "buzz"
+        buzz = 1
+
+    if fizz and buzz == 1 :
+        resultat = "fizzbuzz"
+
     return resultat
 
 if __name__ == '__main__':
